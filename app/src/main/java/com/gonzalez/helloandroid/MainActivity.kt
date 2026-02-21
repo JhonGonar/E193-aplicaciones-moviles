afcd5f4917fca9171cfceed748a409fee553c172
Estiven Rodriguez <estivenalexanderr7@gmail.com>
2026-02-20 21:34:24 -0500

    agregar variables y funciones

--- app/src/main/java/com/gonzalez/helloandroid/MainActivity.kt
+++ app/src/main/java/com/gonzalez/helloandroid/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.gonzalez.helloandroid
 
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -11,10 +14,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.textView)
+        val btnSaludar = findViewById<Button>(R.id.btnSaludar)
+
+        // Variable contador
+        var contador = 0
+// Asignar acción al botón
+        btnSaludar.setOnClickListener {
+            contador++
+            textView.text = "Has hecho clic $contador veces"
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
         }
     }
 }
